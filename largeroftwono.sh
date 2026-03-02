497e6b6d8b70f361372b32d9cca18c223c2f76c4
Bezalel Mathew <bezalelmathew659@gmail.com>
2026-03-02 15:15:20 +0530

    Add files via upload

---
+++ largeroftwono.sh
@@ -0,0 +1,8 @@
+echo "Enter two numbers:"
+read a b
+if [ $a -gt $b ]
+then
+    echo "$a is larger than $b"
+    else
+    echo "$b is larger than $a"
+fi
---
+++ natural.sh
@@ -0,0 +1,6 @@
+echo "Eneter the value of n:"
+read n
+for((i=1; i<=n; i++))
+do
+    echo "$i"\0
+done
