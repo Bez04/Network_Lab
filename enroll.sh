fa332b88a6cdc7c17d8dde884c8ad7233b5818f1
Bezalel Mathew <bezalelmathew659@gmail.com>
2026-03-02 15:47:52 +0530

    Add files via upload

---
+++ enroll.sh
@@ -0,0 +1,11 @@
+echo "Enter your name:"
+read name
+echo "Enter your progam name:"
+read prog
+echo "Enter your  enrollment number:"
+read enroll
+clear
+echo "Details you entered are:"
+echo "Name: $name"
+echo "Program Name: $prog"
+echo "Enrollment Number: $enroll"
