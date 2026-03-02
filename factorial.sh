5e54158f894c8fb6dc027ed5e10479804e38d955
Bezalel Mathew <bezalelmathew659@gmail.com>
2026-03-02 14:25:32 +0530

    Add files via upload

---
+++ factorial.sh
@@ -0,0 +1,19 @@
+echo "Enter a number:"
+read n
+
+j=1
+
+while [ $j -le $n ]
+do
+    f=1
+    i=1
+
+    while [ $i -le $j ]
+    do
+        f=`expr $f \* $i`
+        i=`expr $i + 1`
+    done
+
+    echo "Factorial of $j is $f"
+    j=`expr $j + 1`
+done
